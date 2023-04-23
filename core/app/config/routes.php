8d939da0876fa68c008ca975268c3001e8cdc018
der.leksey@ya.ru <der.leksey@ya.ru>
2023-04-23 22:04:42 +0300

    fix get cors 3

--- core/app/config/routes.php
+++ core/app/config/routes.php
@@ -4,6 +4,7 @@
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
     header('Access-Control-Allow-Headers: token, Content-Type');
     header('Access-Control-Max-Age: 1728000');
--- core/app/controllers/Company.php
+++ core/app/controllers/Company.php
@@ -7,9 +7,46 @@ class Company extends \Zoomx\Controllers\Controller
     /**
      * 
      */
-    public function get()
+    public function new()
     {
-        return jsonx([json_decode(file_get_contents('php://input'))]);
+        $this->modx->addPackage('app', MODX_CORE_PATH . 'app/model/');
+        $q = $this->modx->newQuery('Company');
+        $q->select('*');
+        $q->where([
+
+        ]);
+        $q->sortby('createdon', 'DESC');
+        $q->prepare();
+        $q->stmt->execute();
+        $q_result = $q->stmt->fetchAll(\PDO::FETCH_ASSOC) ?: 0;
+        return $q_result;
+        return jsonx([
+            'success' => true,
+            'data' => $q_result
+        ]);
+    }
+
+
+    /**
+     * 
+     */
+    public function list()
+    {
+        $this->modx->addPackage('app', MODX_CORE_PATH . 'app/model/');
+        $q = $this->modx->newQuery('Company');
+        $q->select('*');
+        $q->where([
+
+        ]);
+        $q->sortby('createdon', 'DESC');
+        $q->prepare();
+        $q->stmt->execute();
+        $q_result = $q->stmt->fetchAll(\PDO::FETCH_ASSOC) ?: 0;
+        return $q_result;
+        return jsonx([
+            'success' => true,
+            'data' => $q_result
+        ]);
     }
 
 }
