90eccb5fd3749308f521fd4f30dd056aaa32e746
trueotmor <proskurin.sasha78@gmail.com>
2023-05-19 18:06:33 +0300

    asda

--- front/src/constants.js
+++ front/src/constants.js
@@ -1,12 +1,12 @@
 const tableColumns = [
-    { name: 'name', label: 'Наименование организации', field: 'name', align: 'left' },
-    { name: 'inn', label: 'ИНН', field: 'inn', align: 'left', hideFrom: 400 },
-    { name: 'vacancy', label: 'Вакансии', field: 'carbs', hideFrom: 650 },
-    { name: 'users', label: 'Пользователи', field: 'users', hideFrom: 800 },
-    { name: 'feedback', label: 'Отклики в работе', field: 'feedback', hideFrom: 900 },
-    { name: 'action', label: 'Действие', field: 'action' },
-    { name: 'user', label: 'Создатель аккаунта', field: 'user', align: 'left', hideFrom: 1000 },
-    { name: 'createdon', label: 'Дата создания', field: 'createdon', align: 'left', hideFrom: 1200 },
+    { name: 'name', label: 'Наименование организации', field: 'name', align: 'left', sortable: true},
+    { name: 'inn', label: 'ИНН', field: 'inn', align: 'left', hideFrom: 600, sortable: false },
+    { name: 'vacancy', label: 'Вакансии', field: 'carbs', hideFrom: 850, sortable: false },
+    { name: 'users', label: 'Пользователи', field: 'users', hideFrom: 1000, sortable: false },
+    { name: 'feedback', label: 'Отклики в работе', field: 'feedback', hideFrom: 1100, sortable: false },
+    { name: 'action', label: 'Действие', field: 'action', sortable: false },
+    { name: 'user', label: 'Создатель аккаунта', field: 'user', align: 'left', hideFrom: 1200, sortable: false },
+    { name: 'createdon', label: 'Дата создания', field: 'createdon', align: 'left', hideFrom: 1400, sortable: false },
 ]
 
 const tableColumnsNames = ['name', 'inn', 'vacancy', 'users', 'feedback', 'action', 'user', 'createdon']
--- front/src/pages/AccountCompanies.vue
+++ front/src/pages/AccountCompanies.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, computed } from 'vue'
 import axios from 'axios'
-import { biThreeDots, biEyeFill, biPencilFill, biX } from '@quasar/extras/bootstrap-icons'
+import { biThreeDots, biEyeFill, biPencilFill, biX, biDashCircle, biPlusCircle } from '@quasar/extras/bootstrap-icons'
 import AccountBase from './AccountBase.vue'
 import { useElementSize } from '@vueuse/core'
 import { tableColumns, tableColumnsNames } from '../constants'
@@ -34,7 +34,16 @@ const visibleColumns = computed(() => {
 <template>
     <account-base>
         <div class="box" ref="box">
-            <q-table :rows="rows" :columns="tableColumns" row-key="name" hide-bottom flat :visible-columns="visibleColumns">
+            <q-table :rows="rows" :columns="tableColumns" row-key="name" hide-bottom flat :visible-columns="visibleColumns"
+                binary-state-sort class="companies__table">
+                <!-- топ -->
+                <template v-slot:top>
+                    <q-input dense debounce="300" color="primary" v-model="filter" bg-color="red">
+                        <template v-slot:append>
+                            <q-icon name="поиск" />
+                        </template>
+                    </q-input>
+                </template>
                 <!-- шапка -->
                 <template v-slot:header="props">
                     <q-tr :props="props">
@@ -49,33 +58,38 @@ const visibleColumns = computed(() => {
                     <q-tr :props="props">
                         <!-- кнопка + -->
                         <q-td auto-width>
-                            <q-btn v-if="tableColumnsNames.length != visibleColumns.length"
-                                @click="props.expand = !props.expand" size="sm" color="primary" round dense>
-                                {{ props.expand ? '—' : '+' }}
-                                <!-- заменить на иконку -->
+                            <q-btn v-if="tableColumnsNames.length != visibleColumns.length && !props.expand"
+                                @click="props.expand = !props.expand" round flat class="companies__table-button">
+                                <q-icon :name="biPlusCircle" color="green-12"
+                                    class=" companies__table-icon companies__table-icon--plus" />
+                            </q-btn>
+                            <q-btn v-if="tableColumnsNames.length != visibleColumns.length && props.expand"
+                                @click="props.expand = !props.expand" round flat class="companies__table-button">
+                                <q-icon :name="biDashCircle" color="red-5"
+                                    class="companies__table-icon companies__table-icon--minus" />
                             </q-btn>
                         </q-td>
-                        <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                        <q-td v-for=" col  in  props.cols " :key="col.name" :props="props">
                             <q-btn-dropdown color="" flat label="" :dropdown-icon="biThreeDots" class="text-primary"
                                 size="10px" padding="10px" no-icon-animation v-if="col.name === 'action'">
                                 <q-list class="companies__menu-list">
-                                    <q-item clickable v-close-popup @click="">
+                                    <q-item clickable v-close-popup @click=" ">
                                         <q-item-section>
                                             <q-item-label>
-                                                <q-icon :name="biEyeFill" />
+                                                <q-icon :name="biEyeFill" color="light-blue-5" />
                                                 Просмотр
                                             </q-item-label>
                                         </q-item-section>
                                     </q-item>
-                                    <q-item clickable v-close-popup @click="">
+                                    <q-item clickable v-close-popup @click=" ">
                                         <q-item-section>
-                                            <q-icon :name="biPencilFill" />
+                                            <q-icon :name="biPencilFill" color="green-12" size="12px" />
                                             <q-item-label> Редактирование</q-item-label>
                                         </q-item-section>
                                     </q-item>
-                                    <q-item clickable v-close-popup @click="">
+                                    <q-item clickable v-close-popup @click=" ">
                                         <q-item-section>
-                                            <q-icon :name="biX" />
+                                            <q-icon :name="biX" color="red-5" size="20px" />
                                             <q-item-label> Удаление</q-item-label>
                                         </q-item-section>
                                     </q-item>
@@ -89,7 +103,7 @@ const visibleColumns = computed(() => {
                         <q-td colspan="100%">
                             <div class="text-left">
                                 <ul>
-                                    <template v-for="i, k in props.row">
+                                    <template v-for=" i, k  in  props.row ">
                                         <!-- если колонка есть, но скрыта -->
                                         <li v-if="tableColumnsNames.includes(k) && !visibleColumns.includes(k)">
                                             {{ k }} {{ i }}
@@ -111,7 +125,6 @@ const visibleColumns = computed(() => {
     border-radius: 0.2em;
     padding: 0.5rem 0;
 
-
     & .q-item {
         padding: 0.6em 1.2em;
 
@@ -126,11 +139,42 @@ const visibleColumns = computed(() => {
                 margin-right: 5px;
                 margin-top: -3px;
             }
+        }
+    }
+}
 
+.companies__table {
+    & .q-table__top {
+        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+    }
+
+    & thead {
+        background: #F6F6F7;
+
+        ::before & th {
+            font-size: 1em;
+            font-weight: 800;
+            color: #4B4B5A;
+            padding: 16px 30px;
         }
 
     }
 
 
+    & th.sortable {
+        direction: rtl;
+    }
+
+    &-button {
+        font-size: 10px;
+
+        & svg {
+            stroke: 2px;
+        }
+
+        & .q-focus-helper {
+            display: none;
+        }
+    }
 }
 </style>
